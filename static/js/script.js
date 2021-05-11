10be7aef19019f032ebcf60eb9bdcce7d9d9fcd7
unknown <azza_5252@hotmail.com>
2021-05-11 21:56:45 +0100

    added function and logic for user input.

--- static/js/script.js
+++ static/js/script.js
@@ -5,8 +5,28 @@ function computerPlay() {
     choices = ["rock", "paper", "scissor"]
 
     computerChosen = choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerChosen)
+    return computerChosen
 }
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+// Checks user input, sets it to lowercase and then confirms it matches the if statement.
+const playerSelection = prompt("Please enter: rock, paper, scissors").toLocaleLowerCase();
+if(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissor") {
+//    Code goes here.
+} else {
+    alert("That's not an option, refresh the page.");
+}
+
+// displays what the user has typed after it's been processed.
+console.log(playerSelection);
+
+
+const computerSelection = computerPlay();
+
 
 // run function
 computerPlay()
